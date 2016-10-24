22217613d0a03b650a50ab805d79f9026228b130
ian <idmwsls@gmail.com>
2016-10-24 21:57:01 +0900

    added necessary operators for UpdateExpressions

--- tests/operators.js
+++ tests/operators.js
@@ -3,33 +3,27 @@ import { expect } from 'chai';
 
 import operators from '../lib/operators';
 
-const validSizeOperator = operators.validSizeOperator;
+const parse = (val) => {
+  if (Array.isArray(val)) return val.map(item => `-${item}`);
+  if (typeof val === 'object') {
+    return Object.entries(val).reduce((string, [key, value]) => `-${key} = ${parse(value)}`, '');
+  }
+  return `-${val}`;
+};
+const parseString = val => `-${val}`;
+const names = {};
 
-const testPath = 'path';
-const testValue = 'value';
-const testValues = ['one', 'two', 'three'];
+const path = 'path';
+const value = 'value';
+const pValue = '-value';
+const values = ['one', 'two', 'three'];
 
 const requiresPath = operator => it('throws an error if no path argument is provided', () => {
-  expect(() => operators[operator].render(null, testValue)).to.throw(`${operator} requires an attribute path`);
+  expect(() => operators[operator](null, value, { parse })).to.throw(`$${operator} requires an attribute path`);
 });
 
 const requiresArray = operator => it('throws an error if value is not an array', () => {
-  expect(() => operators[operator].render(testPath, testValue)).to.throw(`${operator} requires argument type Array`);
-});
-
-const expectShouldParse = (operator, shouldParse) => it(`should have 'shouldParse' set to '${shouldParse}'`, () => {
-  expect(operators[operator].parse).to.equal(shouldParse);
-});
-
-describe('validSizeOperator', () => {
-  it('returns the input without the operator present', () => {
-    expect(validSizeOperator('user.profile.image.$size')).to.equal('user.profile.image');
-  });
-  it('throws an error when passed an invalid size operator path', () => {
-    for (const input of ['$size.thing', 'thing.$size.$size', 'thing.$size.thing']) {
-      expect(() => validSizeOperator(input).to.throw(`invalid use of $size operator '${input}'`));
-    }
-  });
+  expect(() => operators[operator](path, value, { parse })).to.throw(`$${operator} requires argument type Array`);
 });
 
 describe('operators', () => {
@@ -37,106 +31,175 @@ describe('operators', () => {
   for (const [operator, comparator] of Object.entries(comparators)) {
     describe(`.${operator}`, () => {
       it(`joins path and value arguments with "${comparator}"`, () => {
-        expect(operators[operator].render(testPath, testValue)).to.equal(`${testPath} ${comparator} ${testValue}`);
+        expect(operators[operator](path, value, { parse })).to.equal(`${path} ${comparator} ${pValue}`);
       });
       requiresPath(operator);
-      expectShouldParse(operator, true);
     });
   }
 
   const between = 'between';
   describe(`.${between}`, () => {
     it('returns a BETWEEN comparator', () => {
-      expect(operators[between].render(testPath, testValues)).to.equal('path BETWEEN one AND two');
+      expect(operators[between](path, values, { parse })).to.equal('path BETWEEN -one AND -two');
     });
     requiresPath(between);
     requiresArray(between);
-    expectShouldParse(between, true);
   });
 
   const inOp = 'in';
   describe(`.${inOp}`, () => {
     it('returns an IN comparator', () => {
-      expect(operators[inOp].render(testPath, testValues)).to.equal('path IN (one, two, three)');
+      expect(operators[inOp](path, values, { parse })).to.equal('path IN (-one, -two, -three)');
     });
     requiresPath(inOp);
     requiresArray(inOp);
-    expectShouldParse(inOp, true);
   });
 
   const nin = 'nin';
   describe(`.${nin}`, () => {
     it('returns an NOT IN comparator', () => {
-      expect(operators[nin].render(testPath, testValues)).to.equal('NOT path IN (one, two, three)');
+      expect(operators[nin](path, values, { parse })).to.equal('NOT path IN (-one, -two, -three)');
     });
     requiresPath(nin);
     requiresArray(nin);
-    expectShouldParse(nin, true);
   });
 
   const exists = 'exists';
   describe(`.${exists}`, () => {
     it('returns an exists function when value argument is true', () => {
-      expect(operators[exists].render(testPath, true)).to.equal(`attribute_exists(${testPath})`);
+      expect(operators[exists](path, true, { parse })).to.equal(`attribute_exists(${path})`);
     });
     it('returns a not_exists function when value argument is false', () => {
-      expect(operators[exists].render(testPath, false)).to.equal(`attribute_not_exists(${testPath})`);
+      expect(operators[exists](path, false, { parse })).to.equal(`attribute_not_exists(${path})`);
     });
     it('throws an error if value is not a boolean', () => {
-      expect(() => operators[exists].render(testPath, 'not bool')).to.throw(`${exists} requires argument type Boolean`);
+      expect(() => operators[exists](path, 'not bool', { parse })).to.throw(`${exists} requires argument type Boolean`);
     });
     requiresPath(exists);
-    expectShouldParse(exists, false);
   });
 
-  const functionals = { type: 'attribute_type', beginsWith: 'begins_with', contains: 'contains' };
+  const functionals = {
+    type: 'attribute_type',
+    beginsWith: 'begins_with',
+    contains: 'contains',
+    append: 'list_append',
+    ine: 'if_not_exists',
+  };
   for (const [operator, method] of Object.entries(functionals)) {
     describe(`.${operator}`, () => {
       it(`places path and value into "${method}" method`, () => {
-        expect(operators[operator].render(testPath, testValue)).to.equal(`${method}(${testPath}, ${testValue})`);
+        expect(operators[operator](path, value, { parse })).to.equal(`${method}(${path}, -${value})`);
       });
       requiresPath(operator);
-      expectShouldParse(operator, true);
     });
   }
 
+  const prepend = 'prepend';
+  describe(`.${prepend}`, () => {
+    it('places value and path into "append" method', () => {
+      expect(operators[prepend](path, value, { parse })).to.equal(`list_append(-${value}, ${path})`);
+    });
+    requiresPath(prepend);
+  });
+
   const size = 'size';
   describe(`.${size}`, () => {
+    const sizeops = {
+      parseString,
+      names,
+    };
     it('returns size method', () => {
-      expect(operators[size].render(testPath, testValue)).to.equal(`size(${testPath})`);
+      expect(operators[size]('$size:path', sizeops)).to.equal('size(-path)');
     });
-    requiresPath(size);
-    expectShouldParse(size, false);
+    it('throws an error when passed an invalid size operator', () => {
+      for (const input of ['$size.thing', 'thing.$size.$size', 'thing.$size.thing']) {
+        expect(() => operators[size](input, sizeops).to.throw(`invalid use of $size operator '${input}'`));
+      }
+    });
   });
 
   const logicals = { or: 'OR', and: 'AND' };
   for (const [operator, joinder] of Object.entries(logicals)) {
     describe(`.${operator}`, () => {
       it(`joins values with ${joinder}`, () => {
-        expect(operators[operator].render(null, testValues)).to.equal(`one ${joinder} two ${joinder} three`);
+        expect(operators[operator](null, values, { parse })).to.equal(`-one ${joinder} -two ${joinder} -three`);
       });
       it('parenthesizes response when path provided', () => {
-        expect(operators[operator].render(testValue, testValues)).to.equal(`( one ${joinder} two ${joinder} three )`);
+        expect(operators[operator](path, values, { parse })).to.equal(`( -one ${joinder} -two ${joinder} -three )`);
       });
       requiresArray(operator);
-      expectShouldParse(operator, true);
     });
   }
 
   const not = 'not';
   describe(`.${not}`, () => {
     it('negates value with NOT', () => {
-      expect(operators[not].render(testPath, testValue)).to.equal(`NOT ${testValue}`);
+      expect(operators[not](path, value, { parse })).to.equal(`NOT -${value}`);
     });
-    expectShouldParse(not, true);
   });
 
   const nor = 'nor';
   describe(`.${nor}`, () => {
     it('negates parenthesized values joined with OR', () => {
-      expect(operators[nor].render(testPath, testValues)).to.equal('NOT ( one OR two OR three )');
+      expect(operators[nor](path, values, { parse })).to.equal('NOT ( -one OR -two OR -three )');
     });
     requiresArray(nor);
-    expectShouldParse(nor, true);
   });
+
+  const inc = 'inc';
+  describe(`.${inc}`, () => {
+    it('returns a increment expression for positive numbers', () => {
+      expect(operators[inc](path, 5, { parse })).to.equal(`${path} + -5`);
+      expect(operators[inc](path, -5, { parse })).to.equal(`${path} - -5`);
+    });
+    it('throws an error if input is not a number', () => {
+      expect(() => operators[inc](path, value, { parse })).to.throw(`${inc} requires argument type Number`);
+      expect(() => operators[inc](path, NaN, { parse })).to.throw(`${inc} requires argument type Number`);
+    });
+  });
+
+  /* TO-DO: better dummy parse?
+  const set = 'set';
+  describe(`.${set}`, () => {
+    const setops = {
+      parse,
+      parseString,
+      names,
+    };
+    it('returns a set expression', () => {
+      expect(operators[set](path, { path: value }, setops)).to.equal(`SET -${path} = -${value}`);
+    });
+    it('expects an object arg', () => {
+      expect(() => operators[set](path, 'string', setops)).to.throw(`${set} requires argument type Object`);
+    });
+  });
+  */
+
+  const remove = 'remove';
+  describe(`.${remove}`, () => {
+    it('returns a remove expression', () => {
+      expect(operators[remove](path, [value], { parseString, names })).to.equal('REMOVE -value');
+      expect(operators[remove](path, ['one', 'two'], { parseString, names })).to.equal('REMOVE -one, -two');
+    });
+    it('throws an error if arg is not string or array of strings', () => {
+      const expected = `${remove} requires a path or a list of paths`;
+      expect(() => operators[remove](path, 5, { parseString, names })).to.throw(expected);
+      expect(() => operators[remove](path, [5], { parseString, names })).to.throw(expected);
+    });
+  });
+
+  const updaters = ['add', 'delete'];
+  for (const operator of updaters) {
+    describe(`.${operator}`, () => {
+      it(`returns a ${operator} expression`, () => {
+        expect(operators[operator](path, { a: 2, b: 3 }, { parse, parseString, names }))
+        .to.equal(`${operator.toUpperCase()} -a -2, -b -3`);
+      });
+      it('throws an error if arg is not an object', () => {
+        const expected = `${operator} requires argument type Object`;
+        expect(() => operators[operator](path, 5, { parseString, names })).to.throw(expected);
+        expect(() => operators[operator](path, [5], { parseString, names })).to.throw(expected);
+      });
+    });
+  }
 });
--- tests/parser.js
+++ tests/parser.js
@@ -6,7 +6,7 @@ import parseExpression from '../lib/parser';
 describe('parseExpression', () => {
   it('parses input queries into named DynamoDB Expressions with AttributeNames and AttributeValues', () => {
     expect(parseExpression({
-      Expression: { 'name.$size': { $gte: 20 } },
+      Expression: { '$size:name': { $gte: 20 } },
     })).to.deep.equal({
       Expression: 'size(#1) >= :1',
       ExpressionAttributeNames: { '#1': 'name' },
@@ -14,7 +14,7 @@ describe('parseExpression', () => {
     });
 
     expect(parseExpression({
-      Expression: { 'user.accomplishments.count': { $lt: 'ego.$size' } },
+      Expression: { 'user.accomplishments.count': { $lt: '$size:ego' } },
     })).to.deep.equal({
       Expression: '#1.#2.#3 < size(#4)',
       ExpressionAttributeNames: { '#1': 'user', '#2': 'accomplishments', '#3': 'count', '#4': 'ego' },
@@ -63,6 +63,73 @@ describe('parseExpression', () => {
       FilterExpression: '#2 = :2',
       ExpressionAttributeNames: { '#1': 'expired', '#2': 'status' },
       ExpressionAttributeValues: { ':1': true, ':2': 'active' },
+    });
+
+    expect(parseExpression({
+      UpdateExpression: {
+        $set: { price: 10, qty: { $inc: 5 }, rating: { $ine: 0 } },
+        $add: { locations: 'chicago', products: 'paper bag' },
+      },
+    })).to.deep.equal({
+      UpdateExpression: 'SET #1 = :1, #2 = #2 + :2, #3 = if_not_exists(#3, :3) ADD #4 :4, #5 :5',
+      ExpressionAttributeNames: {
+        '#1': 'price',
+        '#2': 'qty',
+        '#3': 'rating',
+        '#4': 'locations',
+        '#5': 'products',
+      },
+      ExpressionAttributeValues: { ':1': 10, ':2': 5, ':3': 0, ':4': 'chicago', ':5': 'paper bag' },
+    });
+
+    expect(parseExpression({
+      UpdateExpression: {
+        $set: {
+          bucketlist: {
+            $append: 'build a house',
+            $prepend: 'paint a self-portrait',
+          },
+        },
+        $remove: ['batteries', 'shoes'],
+        $delete: { friends: 'tom' },
+      },
+      ProjectionExpression: ['bucketlist', 'friends'],
+      ConditionExpression: { profile: 'mine' },
+    })).to.deep.equal({
+      UpdateExpression: 'SET #1 = list_append(#1, :1), #1 = list_append(:2, #1) REMOVE #2, #3 DELETE #4 :3',
+      ProjectionExpression: '#1, #4',
+      ConditionExpression: '#5 = :4',
+      ExpressionAttributeNames: {
+        '#1': 'bucketlist',
+        '#2': 'batteries',
+        '#3': 'shoes',
+        '#4': 'friends',
+        '#5': 'profile',
+      },
+      ExpressionAttributeValues: {
+        ':1': 'build a house',
+        ':2': 'paint a self-portrait',
+        ':3': 'tom',
+        ':4': 'mine',
+      },
+    });
+
+    expect(parseExpression({
+      UpdateExpression: {
+        $set: {
+          '#name.legal.first': { $eq: 'jack' },
+          'accounts.financial.primary': { $inc: -50 },
+        },
+      },
+    })).to.deep.equal({
+      UpdateExpression: 'SET #1 = :1, #2.#3.#4 = #2.#3.#4 - :2',
+      ExpressionAttributeNames: {
+        '#1': 'name.legal.first',
+        '#2': 'accounts',
+        '#3': 'financial',
+        '#4': 'primary',
+      },
+      ExpressionAttributeValues: { ':1': 'jack', ':2': 50 },
     });
   });
 });
